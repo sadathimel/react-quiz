2b4fb70c00773dbb7d58c719e63e3fc840224773
Mahadi001 <sadat.himel@gmail.com>
2021-10-18 12:53:31 +0600

    update

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path = "/" component={Home} />
-          <Route exact path = "/signup" component={Signup} />
-          <Route exact path = "/login" component={Login} />
+          <PublicRoute exact path = "/signup" component={Signup} />
+          <PublicRoute exact path = "/login" component={Login} />
           <PrivateRoute exact path = "/quiz" component={Quiz} />
           <PrivateRoute exact path = "/result" component={Result} />
         </Switch>
