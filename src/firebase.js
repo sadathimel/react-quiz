0eb97b0beb12fb23ce691870dbd090f2aef83b3c
Mahadi001 <sadat.himel@gmail.com>
2021-10-20 15:47:02 +0600

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
-import {initializeApp} from "firebase/app";
 
-// firebase configuration
+import { initializeApp } from "firebase/app";
+
 const app = initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain:  process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL:  process.env.REACT_APP_DATABASE_URL,
-  projectId:  process.env.REACT_APP_PROJECT_ID,
-  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId:  process.env.REACT_APP_MESSAG_ING_SENDER_ID,
-  appId:  process.env.REACT_APP_APP_ID,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 });
 
+
 export default app;
+ 
+ 
