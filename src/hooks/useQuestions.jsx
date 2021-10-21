25a2738b4e8a9305cc9edbec25f47bf0fd8e1b43
Mahadi001 <sadat.himel@gmail.com>
2021-10-21 23:00:44 +0600

    update

---
+++ src/hooks/useQuestions.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { getDatabase, ref, query, orderByKey, get } from "firebase/database";
+import { useState } from "react";
+
+export default function useQuestions(videoID) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      // database related works
+      const db = getDatabase();
+      const quizRef = ref(db, "quiz/" + videoID + "/questions");
+      const quizQuery = query(quizRef, orderByKey());
+
+      try {
+        setError(false);
+        setLoading(true);
+        // request firebase database
+        const snapshot = await get(quizQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setQuestions((prevQuestions) => {
+            return [...prevQuestions, ...Object.values(snapshot.val())];
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    fetchQuestions();
+  }, [videoID]);
+
+  return {
+    loading,
+    error,
+    questions,
+  };
+}
