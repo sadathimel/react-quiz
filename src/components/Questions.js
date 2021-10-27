e1ede6e3837ddcb484ae9f8f59bfbbf9bccf2338
Mahadi001 <sadat.himel@gmail.com>
2021-10-27 17:59:40 +0600

    update

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Answers from './Answers';
-import classes from '../styles/Question.module.css';
+import React from "react";
+import Answers from "./Answers";
+import classes from "../styles/Question.module.css";
 
-
-export default function Question() {
-    return (
-      <div className={classes.question}>
-        <div className={classes.qtitle}>
-          <span className="material-icons-outlined"> help_outline </span>
-          Here goes the question from Learn with Sumit?
-        </div>
-        <Answers />
+export default function Question({ answers = [] }) {
+  return answers.map((answer, index) => (
+    <div className={classes.question} key={index}>
+      <div className={classes.qtitle}>
+        <span className="material-icons-outlined"> help_outline </span>
+        {answer.title}
       </div>
-    );
+      <Answers />
+    </div>
+  ));
 }
